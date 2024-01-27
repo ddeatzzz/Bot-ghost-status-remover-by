3bc056f81494900c03968837289dfd02cc3dbf3a
ddeatzzz <157904768+ddeatzzz@users.noreply.github.com>
2024-01-27 16:07:19 +0000

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Watching RTFL","Made by notddeatz","Watching RTFL"];
 
 
 let currentIndex = 0;
